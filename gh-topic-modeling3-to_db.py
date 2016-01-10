ef8eaef173cf9117e5a7e73c7701dcc712612432
Timo Herttua <timo.herttua@efekta.fi>
2016-01-10 15:26:06 +0200

    minor explorations of the hdp model

--- gh-topic-modeling3-to_db.py
+++ gh-topic-modeling3-to_db.py
@@ -10,7 +10,7 @@ import gensim # needed for text clustering
 from gensim import corpora, models, similarities # needed for text clustering
 import sklearn
 import nltk
-nltk.download() # ensure all the necessary corpora are present for lemmatization
+# nltk.download() # ensure all the necessary corpora are present for lemmatization
 
 # for testing: path = "/Users/timo/Ruby/GetTweets/stored_tweets/2015-05-08.json"
 #path = "/Users/timo/Ruby/GetTweets/stored_tweets/*"
@@ -24,12 +24,25 @@ tweet_texts = sqlContext.sql("SELECT text FROM tweets")
 texts = twpr.run(tweet_texts)
 
 dictionary = corpora.Dictionary(texts)
-dictionary.filter_extremes(no_below=10, no_above=0.25, keep_n=None) # used instead of manually doing this
+dictionary.filter_extremes(no_below=2, no_above=0.5, keep_n=None) # used instead of manually doing this
 corpus = [dictionary.doc2bow(text) for text in texts]
 num_topics = 25
 
+hdp = gensim.models.hdpmodel.HdpModel(corpus, dictionary)
+hdp.print_topics(topics=-1, topn=1)
+
 # above this is common to both using LDA directly from Gensim and running the proprietary functions below
 # lda = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=num_topics, update_every=0, passes=20)
+
+
+
+#  gensim.models.tfidfmodel.TfidfModel(corpus=None, id2word=None, dictionary=None, wlocal=<function identity>, wglobal=<function df2idf>, normalize=True)
+# tfidf = gensim.models.tfidfmodel.TfidfModel(corpus=corpus, dictionary=dictionary)
+# print(tfidf[dictionary.doc2bow(['growth', 'hacking', 'is', 'not', 'the', 'same', 'as', 'a/b', 'testing'])])
+# => [(42, 0.22787410177633208), (306, 0.5049139570055937), (380, 0.5735775649749842), (490, 0.6034435074784973)]
+
+# dictionary.get(5)
+# => 'indiedev'
 
 
 tweet_ids = sqlContext.sql("SELECT id_str as id, text FROM tweets")
