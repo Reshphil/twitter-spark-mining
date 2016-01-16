6ac0f25a6151932920b582b277623000bf27a8f5
Timo Herttua <timo.herttua@efekta.fi>
2016-01-16 18:31:44 +0200

    added comments

--- gh-topic-modeling3.py
+++ gh-topic-modeling3.py
@@ -1,5 +1,6 @@
 '''You might need to use python 3 with pyspark to get gensim, numpy and sklearn properly installed on OS X'''
 
+# point to the folder where the tweets to be analyzed reside
 fs_path = "/Users/timo/Code/spark/"
 
 import processing as twpr
@@ -15,23 +16,39 @@ import nltk
 # for testing: path = "/Users/timo/Ruby/GetTweets/stored_tweets/2015-05-08.json"
 #path = "/Users/timo/Ruby/GetTweets/stored_tweets/*"
 
+# the rest of the path for where the tweets to be analyzed reside
 path = fs_path+"stored_tweets/2015-05-08.json"
+# load tweets into Apache SparkSQL (sqlContext)
 tweets = sqlContext.read.json(path)
 
+# register SparkSQL temptable called 'tweets'
 tweets.registerTempTable("tweets")
+# get the text content of each tweet
 tweet_texts = sqlContext.sql("SELECT text FROM tweets")
+
+# run the processing .run() in the processing.py for the texts
+# as output, we have pre-processed texts ready for gensim dictionary and gensim building
 texts = twpr.run(tweet_texts)
 
+# build Gensim dictionary and corpus with helper methods in processing.py
 dictionary = twpr.buildDictionaryFromTexts(texts)
 corpus = twpr.buildCorpusFromDictionaryAndTexts(texts, dictionary)
 
+# set LDA topic count parameter
 num_topics = 25
+# in order to map LDA output and actual tweets for further analysis, select tweet IDs and texts
 tweet_ids = sqlContext.sql("SELECT id_str as id, text FROM tweets")
-# done
+# now we have all necesssary pre-processed data for LDA analysis
 
+# use the pre-processed inputs to do the LDA analysis
 distros = twpr.doLDA(corpus, dictionary, num_topics, tweet_ids)
-distros_all = distros.collect()
-# done
 
+# now we have the Apache Spark RDD object we can either .take(5) or .collect() all
+distros_all = distros.collect()
+# now we have the LDA topic probability distributions in memory
+
+# to make sense of the LDA output, we need to somehow look at the data
+# thus, we'll write the topics into CSV, weighted with TF-IDF frequencies
 topics = twpr.TFIDFsFromTopicDistributions(distros_all[0:-1], sqlContext, corpus, dictionary)
 twpr.writeTFIDFsToCSV(topics)
+# processing done
